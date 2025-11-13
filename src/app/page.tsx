35c76d326c90e70ab6250a772ebb97cc7e26a0d4
MrF-3097 <francesco.fartonea@gmail.com>
2025-11-13 18:03:04 +0200

    style: add top padding to leaderboard column

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ function TVDisplayContent({ stats, agents }: { stats: any, agents: any }) {
         </div>
 
         {/* Column 2: Leaderboard */}
-        <div className="w-[70%] h-full flex flex-col" style={{ paddingLeft: '50px' }}>
+        <div className="w-[70%] h-full flex flex-col" style={{ paddingLeft: '50px', paddingTop: '10px' }}>
           <GamifiedLeaderboard />
         </div>
       </div>
