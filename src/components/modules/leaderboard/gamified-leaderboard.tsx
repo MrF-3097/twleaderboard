30ff7c34c7bc6ff351a282165f36f5f32cbf6e8d
MrF-3097 <francesco.fartonea@gmail.com>
2025-12-19 18:01:19 +0200

    Remove overflow-hidden from outer leaderboard container
    Removed overflow-hidden completely so the 11th agent can render fully and extend beyond the container boundaries.

--- src/components/modules/leaderboard/gamified-leaderboard.tsx
+++ src/components/modules/leaderboard/gamified-leaderboard.tsx
@@ -241,7 +241,7 @@ export const GamifiedLeaderboard: React.FC = () => {
       </div>
       
       {/* Leaderboard */}
-      <div className={`relative overflow-hidden rounded-2xl ${bgColor} flex-1 flex flex-col min-h-0`}>
+      <div className={`relative rounded-2xl ${bgColor} flex-1 flex flex-col min-h-0`}>
         <div className="relative z-10 flex flex-col min-h-0">
           <CardContent className="flex-1 min-h-0">
             {isLoading && agents.length === 0 ? (
